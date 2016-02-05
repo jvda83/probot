e4d33e36781002a706ffa27b933e2c8bef13bb35
Srdan Crnic <srdancrnic@MacBook.local>
2016-02-05 14:29:31 -0600

    adding javascript

---
+++ js/controllers/MainController.js
@@ -0,0 +1,8 @@
+probotApp.controller('probotApp', function() {
+	this.name = 'Frenzy';
+	this.lift = 'Brutal';
+	this.weight = 500;
+	this.weightUnits = ['lbs', 'kg'];
+	this.inUnits = 'kg';
+
+});
---
+++ js/directives/probotApp.js
@@ -0,0 +1,1 @@
+var probotApp = angular.module('probotApp', [])
