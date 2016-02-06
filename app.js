674dc394a9996066c67848342fbeabcfb4afa718
Srdan Crnic <srdancrnic@MacBook.local>
2016-02-06 15:08:30 -0600

    added remove button and function and added option to add new persons to the board

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 $(document).ready(function() {
 
-var template = function(lift, weight) {
-	return lift + ':' + weight + 'kg<br>';
-};
-
-
-	$('.input').submit(function() {
-		var nameVar = $('#name').val();
+	var templateResult = function(lift, weight) {
+		return '<p>' + lift + ':' + weight + 'kg </p> <button class="remove-btn">Remove</button<br>';
+	};
+	
+	var templatePerson = function(personName) {
+		return '<div class="result-field" id="'+personName+'""><h2>'+personName.toUpperCase()+'</h2></div>'
+	};
+	
+	
+	$('.input-pr').submit(function() {
+		var nameVar = $('#name').val().toLowerCase();
 		var liftVar = $('#lift').val();
 		var weightVar = $('#weight').val();
-		var html = template(liftVar, weightVar);
-
-
+		var html = templateResult(liftVar, weightVar);
+	
 		$('#' + nameVar).append(html);
+		$('#name').val('');
+		$('#lift').val('');
+		$('#weight').val('');
 		return false;
-
+	
 	});
+	
+	$('.input-person').submit(function() {
+		var person = $('#person').val().toLowerCase();
+		var htmlPerson = templatePerson(person);
+
+		$('.results .container').append(htmlPerson);
+		$('#person').val('');
+		return false;
+	});
+
+	$(document).on('click', '.remove-btn', function() {
+		$(this).prev().remove();
+		$(this).remove();
+	});
+
 });
 
