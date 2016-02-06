cfe96bea5953af7b7e69b592eff9f6368b159cd4
Srdan Crnic <srdancrnic@MacBook.local>
2016-02-06 14:04:06 -0600

    changed the code from stupid switch-case to one line of elegant jQuery

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 var template = function(lift, weight) {
 	return lift + ':' + weight + 'kg<br>';
 };
@@ -10,26 +11,8 @@ var template = function(lift, weight) {
 		var weightVar = $('#weight').val();
 		var html = template(liftVar, weightVar);
 
-		switch(nameVar) {
-			case 'Gonzo':
-				$('#gonzo').append(html);
-				break;
-			case 'Max':
-				$('#max').append(html);
-				break;
-			case 'Frenzy':
-				$('#frenzy').append(html);
-				break;
-			case 'Srki':
-				$('#srki').append(html);
-				break;
-			case 'Pinky':
-				$('#pinky').append(html);
-				break;
-			default:
-				alert('Ti nisi clan gerile i odjebi smrtnice!');
 
-		};
+		$('#' + nameVar).append(html);
 		return false;
 
 	});
