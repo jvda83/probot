0c67c696d73419d787f4c37e898cc90577df131d
Srdan Crnic <srdancrnic@MacBook.local>
2016-02-06 11:09:42 -0600

    modified forms and made a VERY PRIMITIVE jquery app to append inputs into result fields

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-var probotApp = angular.module("ProbotApp", []);
+$(document).ready(function() {
+var template = function(lift, weight) {
+	return lift + ':' + weight + 'kg<br>';
+};
+
+
+	$('.input').submit(function() {
+		var nameVar = $('#name').val();
+		var liftVar = $('#lift').val();
+		var weightVar = $('#weight').val();
+		var html = template(liftVar, weightVar);
+
+		switch(nameVar) {
+			case 'Gonzo':
+				$('#gonzo').append(html);
+				break;
+			case 'Max':
+				$('#max').append(html);
+				break;
+			case 'Frenzy':
+				$('#frenzy').append(html);
+				break;
+			case 'Srki':
+				$('#srki').append(html);
+				break;
+			case 'Pinky':
+				$('#pinky').append(html);
+				break;
+			default:
+				alert('Ti nisi clan gerile i odjebi smrtnice!');
+
+		};
+		return false;
+
+	});
+});
 
